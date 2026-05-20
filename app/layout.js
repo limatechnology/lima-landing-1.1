a229803654d394c143c65003beb7a519cbc1afd0
limatechnology <231593324+limatechnology@users.noreply.github.com>
2026-05-20 02:29:12 -0300

    security: fix OWASP ZAP vulnerabilities and add CI header audit

--- app/layout.js
+++ app/layout.js
@@ -101,15 +101,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es">
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PJNCKXDR');
-          `}
-        </Script>
+        <Script id="google-tag-manager" src="/js/gtm.js" strategy="afterInteractive" />
         <meta name="theme-color" content="#101010" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
